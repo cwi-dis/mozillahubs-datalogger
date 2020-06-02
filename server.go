42c7274063404ddeeee32ffadf542d8cd064f9ff
Thomas Röggla <tom@lichtfragmente.com>
2020-06-02 16:27:30 +0200

    reading request body and handling possible errors

--- server.go
+++ server.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 )
 
+func getTimestamp() int64 {
+	now := time.Now()
+	return now.UnixNano()
+}
+
 func handleRequest(writer http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		fmt.Fprintf(writer, "Hello World")
+	if req.Method == http.MethodPost {
+		_, err := ioutil.ReadAll(req.Body)
+
+		if err != nil {
+			log.Printf("Could not read request body")
+
+			msg, _ := json.Marshal(map[string]string{
+				"status": "error",
+			})
+
+			http.Error(writer, string(msg), http.StatusBadRequest)
+			return
+		}
+
+		// TODO parse body and save to file
+
+		msg, _ := json.Marshal(map[string]string{
+			"status": "OK",
+			"type":   "HTTPD",
+			"time":   string(getTimestamp()),
+		})
+
+		fmt.Fprintf(writer, string(msg))
 	}
 }
 
