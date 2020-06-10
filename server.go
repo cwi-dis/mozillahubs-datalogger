63fabd580c34045bacc1f8b5df41aad16308a3e1
Thomas Röggla <tom@lichtfragmente.com>
2020-06-10 16:52:02 +0200

    reformatted code using gofmt

--- server.go
+++ server.go
@@ -34,7 +34,7 @@ func getTimestamp() float64 {
 }
 
 func writeToFile(path string, body *inputData) error {
-  start := time.Now().UnixNano()
+	start := time.Now().UnixNano()
 	outFile, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0744)
 
 	if err != nil {
@@ -66,12 +66,12 @@ func writeToFile(path string, body *inputData) error {
 		outFile.WriteString(infoPart + dataPart[:len(dataPart)-1] + "\n")
 	}
 
-	log.Printf("writeToFile %d", time.Now().UnixNano() - start)
+	log.Printf("writeToFile %d", time.Now().UnixNano()-start)
 	return nil
 }
 
 func parseRequestBody(body *[]byte) (*inputData, error) {
-  start := time.Now().UnixNano()
+	start := time.Now().UnixNano()
 	var bodyData *inputData = &inputData{}
 
 	if err := json.Unmarshal(*body, bodyData); err != nil {
@@ -82,7 +82,7 @@ func parseRequestBody(body *[]byte) (*inputData, error) {
 		return nil, errors.New("Required fields are missing or invalid")
 	}
 
-	log.Printf("parseRequestBody %d", time.Now().UnixNano() - start)
+	log.Printf("parseRequestBody %d", time.Now().UnixNano()-start)
 	return bodyData, nil
 }
 
@@ -90,7 +90,7 @@ func createHandlerWithPath(saveDir string) func(http.ResponseWriter, *http.Reque
 	return func(writer http.ResponseWriter, req *http.Request) {
 		if req.Method == http.MethodPost {
 			log.Println("Processing request")
-      start := time.Now().UnixNano()
+			start := time.Now().UnixNano()
 			var err error
 
 			timestamp := getTimestamp()
@@ -132,7 +132,7 @@ func createHandlerWithPath(saveDir string) func(http.ResponseWriter, *http.Reque
 				Time:   timestamp,
 			})
 
-      log.Printf("createHandlerWithPath %d", time.Now().UnixNano() - start)
+			log.Printf("createHandlerWithPath %d", time.Now().UnixNano()-start)
 			fmt.Fprintf(writer, string(msg))
 		} else {
 			http.NotFound(writer, req)
