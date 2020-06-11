b1fd9ba11461fcba39d122af1ffb827b6e323f4a
Thomas Röggla <tom@lichtfragmente.com>
2020-06-11 15:48:13 +0200

    added script for testing the server using random data

---
+++ random_test.py
@@ -0,0 +1,40 @@
+import json
+import requests
+import sys
+
+from random import choices, randint, random
+
+
+def make_request(host):
+    data = []
+
+    for _ in range(randint(1, 400)):
+        data.append([
+            "".join(choices("abcdefghijklmnopqrstuvwxzy", k=10)),
+            random(),
+            random()
+        ])
+
+    requests.post(
+        host,
+        data=json.dumps({
+            "info": ["huh", randint(1, 100), randint(1, 100)],
+            "data": data
+        })
+    )
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("USAGE:", sys.argv[0], "endpoint")
+        sys.exit(1)
+
+    repeat = 3000
+
+    for i in range(repeat):
+        print(f"Sending request {i+1}/{repeat}...")
+        make_request(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
