d8ef5396ddff93b3ca7d9b14fbc3ddc6262024d9
Thomas Röggla <t.roggla@cwi.nl>
2022-11-29 16:59:12 +0100

    added HTTP handler for retrieving the timestamp of the latest sample

--- server/server.go
+++ server/server.go
@@ -12,23 +12,30 @@ import (
 	"github.com/cwi-dis/mozillahubs-datalogger/util"
 )
 
-// SuccessResponse is the structure of the data that is returned as JSON upon
-// a successfully completed request.
+// SuccessResponse represents the structure of the data that is returned as
+// JSON upon a successfully completed request.
 type SuccessResponse struct {
 	Status string  `json:"status"`
 	Time   float64 `json:"time"`
 }
 
-// ErrorResponse is the structure of the data that is returned as JSON when the
-// request encountered an error.
+// LastSampleResponse represents the structure of the data that is returned as
+// JSON when requesting the timestamp of the lastest sample
+type LastSampleResponse struct {
+	Status     string `json:"status"`
+	LastSample string `json:"lastSample"`
+}
+
+// ErrorResponse represents the structure of the data that is returned as JSON
+// when the request encountered an error.
 type ErrorResponse struct {
 	Status string `json:"status"`
 }
 
-// CreateHandlerWithPath returns a HTTP handler function which reacts to POST
+// CreateLogHandler returns a HTTP handler function which reacts to POST
 // requests, takes the body data, parses it as JSON and writes the resulting
 // data to a file at the path which is given by a parameter to this function.
-func CreateHandlerWithPath(saveDir string) func(http.ResponseWriter, *http.Request) {
+func CreateLogHandler(saveDir string) func(http.ResponseWriter, *http.Request) {
 	// Initialise mutex to guaranteee exclusive access to output file
 	var mutex = &sync.Mutex{}
 
@@ -91,18 +98,53 @@ func CreateHandlerWithPath(saveDir string) func(http.ResponseWriter, *http.Reque
 	}
 }
 
+// CreateLastUpdateHandler returns a HTTP handler function that responds to GET
+// requests to retrieve the timestamp of the last sample that was received.
+func CreateLastUpdateHandler(saveDir string) func(http.ResponseWriter, *http.Request) {
+	// Return HTTP handler function
+	return func(writer http.ResponseWriter, req *http.Request) {
+		// Make sure the request if a GET request
+		if req.Method != http.MethodGet {
+			msg, _ := json.Marshal(&ErrorResponse{Status: "Method not allowed"})
+			http.Error(writer, string(msg), http.StatusMethodNotAllowed)
+
+			return
+		}
+
+		// Get timestamp of last sample
+		latestSampleTimestamp, err := util.LatestSampleTimestamp(saveDir)
+
+		// Return HTTP 500 if there was an error retrieving the sample
+		if err != nil {
+			msg, _ := json.Marshal(&ErrorResponse{Status: "Could not retrieve timestamp"})
+			http.Error(writer, string(msg), http.StatusInternalServerError)
+		}
+
+		// Generate response JSON with timestamp
+		msg, _ := json.Marshal(&LastSampleResponse{
+			Status:     "OK",
+			LastSample: latestSampleTimestamp.Format(time.RFC3339Nano),
+		})
+
+		// Write response to client
+		fmt.Fprint(writer, string(msg))
+	}
+}
+
 // StartServer starts a new HTTP server at the given port which saves data
 // received from clients in the given directory under an automatically
 // generated name. The server mounts a handler for the route POST /mozillahubs
+// to store the data and a handler for retrieving the timestamp of the latest
+// stored sample under GET /latest
 func StartServer(saveDir string, port int) {
 	portSpec := fmt.Sprintf(":%d", port)
 
 	log.Printf("Server listening on port %d", port)
 
-	http.HandleFunc("/mozillahubs", CreateHandlerWithPath(saveDir))
-	err := http.ListenAndServe(portSpec, nil)
+	http.HandleFunc("/mozillahubs", CreateLogHandler(saveDir))
+	http.HandleFunc("/latest", CreateLastUpdateHandler(saveDir))
 
-	if err != nil {
+	if err := http.ListenAndServe(portSpec, nil); err != nil {
 		log.Println(err)
 	}
 }
